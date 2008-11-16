364ab9f26a65f1755c1b9884afe06288192eb40d
Chris Wanstrath <chris@ozmm.org>
2008-11-16 03:42:19 -0800

    simple campfire notifier

---
+++ integrity-campfire.gemspec
@@ -0,0 +1,19 @@
+Gem::Specification.new do |s|
+  s.name              = 'integrity-email'
+  s.version           = '1.0.0'
+  s.date              = '2008-11-16'
+  s.summary           = 'Campfire notifier for the Integrity continuous integration server'
+  s.description       = 'Easily let Integrity alert Campfire after each build'
+  s.homepage          = 'http://integrityapp.com'
+  s.email             = 'chris@ozmm.org'
+  s.authors           = ['Chris Wanstrath']
+  s.has_rdoc          = false
+  s.files             = %w(
+                          README.markdown
+                          lib/notifier/config.haml
+                          lib/notifier/campfire.rb
+                        )
+
+  s.add_dependency 'foca-integrity'
+  s.add_dependency 'tinder'
+end
---
+++ lib/notifier/campfire.rb
@@ -0,0 +1,39 @@
+require 'rubygems'
+require 'integrity'
+require 'tinder'
+
+module Integrity
+  class Notifier
+    class Campfire < Notifier::Base
+      attr_reader :config
+
+      def self.to_haml
+        File.read File.dirname(__FILE__) / "config.haml"
+      end
+
+      def deliver!
+        campfire = Tinder::Campfire.new(config['account'])
+        campfire.login(config['user'], config['pass'])
+        room = campfire.find_room_by_name(config['room'])
+
+        room.speak "#{short_message}. #{build_url}"
+        room.paste full_message
+      end
+
+      def short_message
+        "Build #{build.short_commit_identifier} of #{build.project.name} #{build.successful? ? "was successful" : "failed"}"
+      end
+
+      def full_message
+        <<-EOM
+Commit Message: #{build.commit_message}
+Commit Date: #{build.commited_at}
+Commit Author: #{build.commit_author.name}
+
+#{stripped_build_output}
+EOM
+      end
+
+    end
+  end
+end
