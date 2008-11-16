a4e02491de213f01bf340d8e0038a8a7816345fe
Chris Wanstrath <chris@ozmm.org>
2008-11-16 03:52:15 -0800

    simple tests

---
+++ test/campfire_test.rb
@@ -0,0 +1,25 @@
+require File.dirname(__FILE__) + '/helper'
+
+context "The Campfire notifier" do
+  setup do
+    @config = {}
+    @notifier = Integrity::Notifier::Campfire
+  end
+
+  test "alerts Campfire on build" do
+    @notifier.any_instance.expects(:room).at_least_once.returns mock \
+      :speak => true,
+      :paste => true
+
+    @notifier.any_instance.expects(:full_message).returns('')
+    @notifier.any_instance.expects(:short_message).returns('')
+    @notifier.any_instance.expects(:build_url).returns('')
+
+    @notifier.notify_of_build(stub_everything(:project => stub_everything), @config)
+  end
+
+  test "renders a haml config file" do
+    haml = Integrity::Notifier::Campfire.to_haml
+    assert haml.include? 'campfire_notifier_account'
+  end
+end
